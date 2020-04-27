572d8eae7934d8c23f5de776602278a3688525d3
tectanagrand <rtektano@gmail.com>
2020-04-28 02:49:53 +0700

    login page, baru interface

--- app/src/main/java/com/kelompokrpl/parkuy/RegisterPage.java
+++ app/src/main/java/com/kelompokrpl/parkuy/RegisterPage.java
@@ -1,16 +1,15 @@
 package com.kelompokrpl.parkuy;
 
 import android.os.Bundle;
-import android.os.PersistableBundle;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
-public class LoginPage extends AppCompatActivity {
+public class RegisterPage extends AppCompatActivity {
 
     @Override
-    public void onCreate(@Nullable Bundle savedInstanceState, @Nullable PersistableBundle persistentState) {
-        super.onCreate(savedInstanceState, persistentState);
-        setContentView(R.layout.registerpage);
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.loginpage);
     }
 }
