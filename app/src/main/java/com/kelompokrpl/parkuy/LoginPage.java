2d439ecb3e70a1efa7b71d017277b0c53209fd3b
tectanagrand <rtektano@gmail.com>
2020-05-08 14:59:36 +0700

    landing_page ; login_page ; retrofit nanti diganti laravel

--- app/src/main/java/com/kelompokrpl/parkuy/LoginPage.java
+++ app/src/main/java/com/kelompokrpl/parkuy/LoginPage.java
@@ -1,19 +1,18 @@
 package com.kelompokrpl.parkuy;
 
+import androidx.appcompat.app.AppCompatActivity;
+
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.LinearLayout;
 
-import androidx.annotation.Nullable;
-import androidx.appcompat.app.AppCompatActivity;
-
-public class RegisterPage extends AppCompatActivity implements View.OnClickListener{
+public class LoginPage extends AppCompatActivity implements View.OnClickListener {
 
     @Override
-    public void onCreate(@Nullable Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.registerpage);
+        setContentView(R.layout.activity_login_page);
 
         LinearLayout btnBack;
         btnBack = findViewById(R.id.btn_back);
@@ -24,7 +23,7 @@ public class RegisterPage extends AppCompatActivity implements View.OnClickListe
     public void onClick(View v) {
         switch(v.getId()){
             case R.id.btn_back:
-                Intent moveBack = new Intent(RegisterPage.this, LandingPage.class);
+                Intent moveBack = new Intent(LoginPage.this, LandingPage.class);
                 startActivity(moveBack);
                 break;
         }
