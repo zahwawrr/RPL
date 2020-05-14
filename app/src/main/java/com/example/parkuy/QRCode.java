5aeda0e6470e242eff479789e5a71f791644bde4
zahwawrr <zahwa_riana31@apps.ipb.ac.id>
2020-05-14 13:01:13 +0700

    Unfinished QR Code

---
+++ app/src/main/java/com/example/parkuy/QRCode.java
@@ -0,0 +1,14 @@
+package com.example.parkuy;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class QRCode extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_q_r_code);
+    }
+}
