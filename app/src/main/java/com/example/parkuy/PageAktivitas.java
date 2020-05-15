a1148a9399b8527c0e27686e2c30966f9f0411d7
zahwawrr <zahwa_riana31@apps.ipb.ac.id>
2020-05-16 00:52:56 +0700

    Modal bottom sheet pop up (akhiri parkir)

--- app/src/main/java/com/example/parkuy/PageAktivitas.java
+++ app/src/main/java/com/example/parkuy/PageAktivitas.java
@@ -1,14 +1,15 @@
 package com.example.parkuy;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
+import androidx.appcompat.app.AppCompatActivity;
+
 public class PageAktivitas extends AppCompatActivity {
     private Button tombolback;
+    private Button tombolakhiriparkir;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,10 +17,19 @@ public class PageAktivitas extends AppCompatActivity {
         setContentView(R.layout.activity_);
 
         tombolback = (Button) findViewById(R.id.tombolback);
-        tombolback.setOnClickListener(new View.OnClickListener(){
+        tombolback.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v){
+            public void onClick(View v) {
                 openHomeScreen();
+            }
+        });
+
+        tombolakhiriparkir = (Button) findViewById(R.id.tombolakhiriparkir);
+        tombolakhiriparkir.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                modal_bottom_akhiriparkir bottom_akhiriparkir = new modal_bottom_akhiriparkir();
+                bottom_akhiriparkir.show(getSupportFragmentManager(), "modalMenu");
             }
         });
     }
---
+++ app/src/main/java/com/example/parkuy/modal_bottom_akhiriparkir.java
@@ -0,0 +1,21 @@
+package com.example.parkuy;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+import com.google.android.material.bottomsheet.BottomSheetDialogFragment;
+
+public class modal_bottom_akhiriparkir extends BottomSheetDialogFragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState){
+        View v = inflater.inflate(R.layout.activity_modal_bottom_akhiriparkir, container, false);
+        return v;
+    }
+}
