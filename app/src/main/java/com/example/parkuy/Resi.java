0ebf4f503e2a79407116e4f2294b52ad82f12f30
zahwawrr <zahwa_riana31@apps.ipb.ac.id>
2020-05-16 03:05:11 +0700

    Resi baru mau dibuat

---
+++ app/src/main/java/com/example/parkuy/Resi.java
@@ -0,0 +1,14 @@
+package com.example.parkuy;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Resi extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.resiparkir);
+    }
+}
--- app/src/main/java/com/example/parkuy/modal_bottom_akhiriparkir.java
+++ app/src/main/java/com/example/parkuy/modal_bottom_akhiriparkir.java
@@ -1,9 +1,11 @@
 package com.example.parkuy;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -16,6 +18,23 @@ public class modal_bottom_akhiriparkir extends BottomSheetDialogFragment {
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState){
         View v = inflater.inflate(R.layout.activity_modal_bottom_akhiriparkir, container, false);
+
+        Button btnbatal = (Button)v.findViewById(R.id.tombolbackbatal);
+        btnbatal.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getActivity(),PageAktivitas.class));
+            }
+        });
+
+        Button btnakhiri = (Button)v.findViewById(R.id.tombolakhiriparkir2);
+        btnbatal.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getActivity(),Resi.class));
+            }
+        });
         return v;
     }
 }
+
